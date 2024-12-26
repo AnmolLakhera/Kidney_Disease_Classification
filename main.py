56cbfd4bc8281da4f349890192cd9c811b33eb16
AnmolLakhera <anmollakhera019@gmail.com>
2024-12-26 15:50:28 +0530

    updated main (commented out model training part to not train model again and again)

--- main.py
+++ main.py
@@ -4,22 +4,15 @@ from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelT
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-
-
-
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-except Exception as e:
-        logger.exception(e)
-        raise e
-
-
-
-
+# STAGE_NAME = "Data Ingestion stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = DataIngestionTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
 
 STAGE_NAME = "Prepare base model"
 try: 
@@ -32,8 +25,6 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-
-
 STAGE_NAME = "Training"
 try: 
    logger.info(f"*******************")
@@ -44,9 +35,6 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-
-
 
 STAGE_NAME = "Evaluation stage"
 try:
@@ -59,5 +47,3 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
-
-
